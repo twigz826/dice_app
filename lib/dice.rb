91d0f3be47e0a9ae4c9d273301d627ed77f000b4
twigz826 <thomastwigden@gmail.com>
2021-01-08 20:15:45 -0300

    Completed tests for all user stories

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,6 +1,11 @@
 class Dice
   def roll(num_of_dice = 1)
-    rand((1*num_of_dice)..(6*num_of_dice))
-    #num_of_dice.times { rand(6) + 1 }
+    score = []
+    num_of_dice.times { score << dice_roll }
+    return score
+  end
+
+  def dice_roll
+    rand(6) + 1
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -7,15 +7,23 @@ describe Dice do
   end
 
   it "gives a number between 1 and 6 when rolled" do
-    expect(subject.roll(1)).to be_between(1, 6)
+    expect(subject.roll(1)).to all(be_between(1, 6))
   end
   
   it "allows me to roll any number of dice at the same time" do
     number_of_dice = 5
-    expect(subject.roll(number_of_dice)).to be_between(5, 30)
+    expect(subject.roll(number_of_dice)).to all(be_between(1, 6))
   end
 
   it "rolls a dice" do
     expect(subject).to respond_to(:roll).with(1).argument
+  end
+
+  it "gives the right number of results" do
+    expect(subject.roll(7).size).to eq 7
+  end
+
+  it "gives results between 1 and 6 for each roll" do
+    expect(subject.roll).to all (be_between(1,6))
   end
 end
